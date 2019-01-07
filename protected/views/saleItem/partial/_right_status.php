e353dc147f44ff94c7bc17474ab4233a83f2e64d
Sopheap <sopheaptep@gmail.com>
2019-01-07 20:45:30 +0700

    Update Button new or edit order

--- protected/views/saleItem/partial/_right_status.php
+++ protected/views/saleItem/partial/_right_status.php
@@ -5,9 +5,8 @@
         <?= $time_go; ?>
     </span>
 <?php } ?>
-<?php if ($ordering_status=='1') { ?>
+<?php if (Yii::app()->orderingCart->getTableId()) { ?>
     <?php if(Yii::app()->user->checkAccess('sale.edit')) { ?>
-
         <?php if(isset($view)){
             if($view=='cashier') {
                 ?>
@@ -16,9 +15,23 @@
                     'size' => TbHtml::BUTTON_SIZE_MINI,
                     'icon' => ' ace-icon fa fa-pencil-square-o white',
                     'class' => 'btn-edit-order',
-                    //'url' => Yii::app()->createUrl('SaleItem/confirmOrder'),
                     'url' => Yii::app()->createUrl('SaleItem/editOrdered'),
                     'title' => Yii::t('app', 'Edit Ordered'),
+                )); ?>
+            <?php }} ?>
+    <?php } ?>
+<?php }else{ ?>
+    <?php if(Yii::app()->user->checkAccess('sale.edit')) { ?>
+        <?php if(isset($view)){
+            if($view=='cashier') {
+                ?>
+                <?php echo TbHtml::linkButton(Yii::t('app', 'New Order'), array(
+                    'color' => TbHtml::BUTTON_COLOR_PRIMARY,
+                    'size' => TbHtml::BUTTON_SIZE_MINI,
+                    'icon' => ' ace-icon fa fa-pencil-square-o white',
+                    'class' => 'btn-edit-order',
+                    'url' => Yii::app()->createUrl('SaleItem/index'),
+                    'title' => Yii::t('app', 'New Order'),
                 )); ?>
             <?php }} ?>
     <?php } ?>
@@ -41,16 +54,19 @@
             'title' => Yii::t('app', 'Confirm Order'),
         )); ?>
 
-    <?php }else{ ?>
-        <?php echo TbHtml::linkButton(Yii::t('app', 'Summary'), array(
-            'color' => TbHtml::BUTTON_COLOR_INFO,
-            'size' => TbHtml::BUTTON_SIZE_MINI,
-            'icon' => ' ace-icon fa fa-eye white',
-            'class' => 'btn-confirm-order',
-            'url' => Yii::app()->createUrl('SaleItem/cashier'),
-            'title' => Yii::t('app', 'Summary Screen'),
-        )); ?>
-    <?php } ?>
+    <?php }//else{ ?>
+
+    <?php //} ?>
+
+
 
 <?php } ?>
+<?php echo TbHtml::linkButton(Yii::t('app', 'Summary'), array(
+    'color' => TbHtml::BUTTON_COLOR_INFO,
+    'size' => TbHtml::BUTTON_SIZE_MINI,
+    'icon' => ' ace-icon fa fa-eye white',
+    'class' => 'btn-confirm-order',
+    'url' => Yii::app()->createUrl('SaleItem/cashier'),
+    'title' => Yii::t('app', 'Summary Screen'),
+)); ?>
 
